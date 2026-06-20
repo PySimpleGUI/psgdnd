e9b936f137949fb2340fae99658cd796512586f9
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2026-06-20 13:40:33 -0400

    Bump version to match the code

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setuptools.setup(
 name="psgdnd",
-version="6.0.2",
+version="6.0.3",
 author="PySimpleGUI",
 install_requires=["PySimpleGUI","tkinterdnd2"],
 description="Drag and Drop expansion for PySimpleGUI",
